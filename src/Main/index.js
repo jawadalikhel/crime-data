05734a4eeded4777cda46f946ea3351c9036054a
Your Name <jawadalikhel18@gmail.com>
2019-01-13 01:55:31 -0600

    changed folder name and added nav component

---
+++ src/Main/index.js
@@ -0,0 +1,14 @@
+import React, {Component} from 'react';
+import Nav from './Nav';
+
+class Main extends Component{
+  render(){
+    return(
+      <div>
+        <Nav />
+      </div>
+    )
+  }
+}
+
+export default Main;
